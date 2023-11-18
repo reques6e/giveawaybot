7f45e67f65a30e99b2c32f55c4943dd185c080bc
Alexander <149709871+reques6e@users.noreply.github.com>
2023-11-18 10:26:04 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import nextcord
+import os
+from nextcord.ext import commands
+
+intents = nextcord.Intents.all()
+bot = commands.Bot(command_prefix='$$$', intents=intents)
+bot.remove_command('help')
+
+for fn in os.listdir("./cogs"):
+	if fn.endswith(".py"):
+		bot.load_extension(f"cogs.{fn[:-3]}")
+
+@bot.command()
+async def load(ctx, extension):
+	bot.load_extension(extension)
+	await ctx.send("Подключил cogs")
+	
+bot.run('YourToken')
